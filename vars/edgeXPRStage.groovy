2d0a8c0ed0a3a25c39ba249c4f787a36465d575e
Ernesto Ojeda <ernesto.ojeda@intel.com>
2019-03-26 09:55:51 -0700

    Testing updated log message to include branchName
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- vars/edgeXPRStage.groovy
+++ vars/edgeXPRStage.groovy
@@ -1,11 +1,11 @@
 def call(branchName = env.GIT_BRANCH, body) {
     // what defines a main release branch
-    def releaseStreams = [/.*master/, /.*california/, /.*delhi/, /.*edinburgh/, /.*git-semver/]
+    def releaseStreams = [/.*master/, /.*california/, /.*delhi/, /.*edinburgh/] //, /.*git-semver/
 
     if(branchName && (releaseStreams.collect { branchName =~ it ? true : false }).contains(true)) {
-        println "[edgeXPRStage] Current branch IS a valid release branch, skipping code..."
+        println "[edgeXPRStage] Current branch [${branchName}] IS a valid release branch, skipping code..."
     } else {
-        println "[edgeXPRStage] Not a release branch, must be in a feature branch or PR"
+        println "[edgeXPRStage] Not a release [${branchName}] branch, must be in a feature branch or PR"
         body()
     }
 }
