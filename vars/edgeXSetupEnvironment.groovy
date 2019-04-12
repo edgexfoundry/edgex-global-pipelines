dd49790216755583aaf606d70b9985c70c730bf0
Ernesto Ojeda <ernesto.ojeda@intel.com>
2019-04-12 11:04:13 -0700

    add ability to call with null git vars
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- vars/edgeXSetupEnvironment.groovy
+++ vars/edgeXSetupEnvironment.groovy
@@ -15,13 +15,24 @@
 //
 
 def call(vars) {
+    if (vars == null) {
+        def gitEnvVars = ['GIT_BRANCH', 'GIT_COMMIT']
+        vars = [:]
+        gitEnvVars.each { var ->
+            vars[var] = env.getProperty(var)
+        }
+    }
+
     if(vars != null) {
         vars.each { k, v ->
             env.setProperty(k, v)
+            
             if(k == 'GIT_BRANCH') {
                 env.setProperty('SEMVER_BRANCH', v.replaceAll( /^origin\//, '' ))
                 env.setProperty('GIT_BRANCH_CLEAN', v.replaceAll('/', '_'))
-            } else if(k == 'GIT_COMMIT') {
+            }
+            
+            if(k == 'GIT_COMMIT') {
                 env.setProperty('SHORT_GIT_COMMIT', env.GIT_COMMIT.substring(0,7))
             }
         }
