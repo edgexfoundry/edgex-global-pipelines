bb1b163ff7d3f3e64e3b3c6c35e6ddc7c3fb1ee3
Vishwas SP <vishwasx.s.premachandra@intel.com>
2020-03-11 14:19:19 -0700

    US5858: EdgeX DevOps: figure out how to lint groovy code
    Signed-off-by: Vishwas SP <vishwasx.s.premachandra@intel.com>

---
+++ scripts/linter.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+JENKINS_URL=https://jenkins.edgexfoundry.org
+JENKINS_CRUMB=`curl --silent "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
+JENKINS_VAL="$JENKINS_URL/pipeline-model-converter/validate"
+JENKINS_FILE_LIST=(`grep "pipeline" vars/* | grep -v \/\/ | awk '{print $1}' | sed -e 's/\://g'`)
+
+for JENKINS_FILE in "${JENKINS_FILE_LIST[@]}"
+do
+	ret=`curl --silent -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$JENKINS_FILE" $JENKINS_VAL`
+	if [[ $ret == *"Errors"* ]];then
+		echo "Linting has failed for $JENKINS_FILE"
+		exit 1
+	else
+		echo "$JENKINS_FILE successfully validated"
+	fi
+done
