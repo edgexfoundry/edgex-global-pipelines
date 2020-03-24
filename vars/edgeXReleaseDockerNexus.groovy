f6b55750fb3643703e25ef5cc2e38f1e467bfc2e
Venkata Lakshmi Dhatri Penna <venkata.lakshmi.dhatrix.penna@intel.com>
2020-03-24 13:18:13 -0700

    Update the functions to publish images from staging to release
    Signed-off-by: Venkata Lakshmi Dhatri Penna <venkata.lakshmi.dhatrix.penna@intel.com>

--- vars/edgeXReleaseDockerNexus.groovy
+++ vars/edgeXReleaseDockerNexus.groovy
@@ -12,11 +12,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-
+// Define parameters
 def call (config) {
-
-    sh "echo Nexus Docker Image Publish"
-    println "dockerSource: ${config.dockerSource}" 
-    
+    def _branchName = config.branchName ?: 'master'
+    def _version = config.version ?: 'v0.0.1-test'
+    def _dockerNexusURL = config.dockerNexusURL ?: 'nexus3edgexfoundry.org'
+    def _dockerImageRepo = config.dockerImageRepo ?: null
+    def _from = config.from ?: 'staging'
+    def _to = config.to ?: 'release'
+    def nexusPortMapping = [
+        snapshots: 10003,
+        snapshot: 10003,
+        staging: 10004,
+        release: 10002
+    ]
+    def sourceNexusPort = nexusPortMapping[_from]
+    def destinationNexusPort = nexusPortMapping[_to]
+    if(!_dockerImageRepo) {
+        throw new Exception('Docker image repository (dockerImageRepo) is required.')
+    }
+    // Pull image from nexus soure repository
+    sh "docker pull ${_dockerNexusURL}:${sourceNexusPort}/${_dockerImageRepo}:${_branchName}"
+    // Get image ID from pulled image
+    def imageID = sh(script: "docker image -q ${_dockerImageRepo}:${_branchName}" returnStdout: true)
+    // Retag pulled image
+    sh "docker tag ${imageID} ${_dockerNexusURL}:${destinationNexusPort}/${_dockerImageRepo}:${_version}"
+    // Push new image
+    sh "docker push ${_dockerNexusURL}:${destinationNexusPort}/${_dockerImageRepo}:${_version}"
 }
