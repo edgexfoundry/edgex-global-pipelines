7fa99f5eee2bf11bc7a51173e80b8b711ec4cfec
Ernesto Ojeda <ernesto.ojeda@intel.com>
2020-09-03 15:06:54 -0700

    feat: add new option for Go 1.15 base image
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- vars/edgex.groovy
+++ vars/edgex.groovy
@@ -211,7 +211,8 @@ def getGoLangBaseImage(version, alpineBased) {
         def goBaseImages = [
             '1.11': 'nexus3.edgexfoundry.org:10003/edgex-devops/edgex-golang-base:1.11.13-alpine',
             '1.12': 'nexus3.edgexfoundry.org:10003/edgex-devops/edgex-golang-base:1.12.14-alpine',
-            '1.13': 'nexus3.edgexfoundry.org:10003/edgex-devops/edgex-golang-base:1.13-alpine'
+            '1.13': 'nexus3.edgexfoundry.org:10003/edgex-devops/edgex-golang-base:1.13-alpine',
+            '1.15': 'nexus3.edgexfoundry.org:10003/edgex-devops/edgex-golang-base:1.15-alpine'
         ]
 
         baseImage = goBaseImages[version]
