1e61f79a8d0d090d39395c2f1f82d426472d7232
Chinu Joy <chinu.joy@intel.com>
2020-11-02 12:38:44 -0800

    fix: Commit message in gh-pages branch
    Signed-off-by: Chinu Joy <chinu.joy@intel.com>

--- src/test/groovy/edgeXGHPagesPublishSpec.groovy
+++ src/test/groovy/edgeXGHPagesPublishSpec.groovy
@@ -17,25 +17,28 @@ public class EdgeXGHPagesPublishSpec extends JenkinsPipelineSpecification {
             def environmentVariables = [
                 'DRY_RUN': 'true'
             ]
+            getPipelineMock('sh')([script: 'git log --format=%B -n 1 | grep -v Signed-off-by | ' +
+                'head -n 1', returnStdout: true]) >> 'Merge PR'
             edgeXGHPagesPublish.getBinding().setVariable('env', environmentVariables)
         when:
             edgeXGHPagesPublish(repoUrl: "${repoUrl}", credentialId: "${credentialId}")
         then:
             1 * getPipelineMock('dir').call(_) >> { _arguments ->
-                assert 'edgex-docs-clean' == _arguments[0][0]
+                assert 'gh-pages-src' == _arguments[0][0]
             }
             1 * getPipelineMock("unstash").call('site-contents') >> true
             1 * getPipelineMock('withEnv').call(_) >> { _arguments ->
                 def envArgs = [
-                    'DRY_RUN=true'
+                    'DRY_RUN=true',
+                    'GH_PAGES_BRANCH=gh-pages',
+                    'COMMIT_MSG=Merge PR'
                 ]
                 assert envArgs == _arguments[0][0]
             }
             1 * getPipelineMock('sshagent').call(_) >> { _arguments ->
                 assert ['credentials': ['test-user']] == _arguments[0][0]
             }
-            1 * getPipelineMock("libraryResource").call('github-pages-publish.sh')
-                >> 'edgex-gh-pages-script'
+            1 * getPipelineMock("libraryResource").call('github-pages-publish.sh') >> 'edgex-gh-pages-script'
             1 * getPipelineMock('sh').call(script: 'edgex-gh-pages-script')
             1 * getPipelineMock("git.call").call(
                 [url : "${repoUrl}", branch: 'gh-pages', credentialsId: "${credentialId}", changelog: false,
@@ -45,8 +48,7 @@ public class EdgeXGHPagesPublishSpec extends JenkinsPipelineSpecification {
     def "Test edgeXGHPagesPublish [Should] return error [When] failed with an exception"() {
         setup:
             def exe = new Exception("Error")
-            getPipelineMock("libraryResource").call('github-pages-publish.sh')
-                >> 'edgex-gh-pages-script'
+            getPipelineMock("libraryResource").call('github-pages-publish.sh') >> 'edgex-gh-pages-script'
             getPipelineMock('sh').call(script: 'edgex-gh-pages-script')
             getPipelineMock('sshagent').call(_) >> { throw exe }
             getPipelineMock("git.call").call(
