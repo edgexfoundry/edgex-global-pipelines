7dbd26b950d1e5298221ce2212768876d1bd982a
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-10-12 18:15:21 -0700

    fix: patch for alpine:3.14 seccomp issue
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- vars/edgeXBuildGoApp.groovy
+++ vars/edgeXBuildGoApp.groovy
@@ -325,6 +325,8 @@ def call(config) {
                             stage('Prep') {
                                 steps {
                                     script {
+                                        edgex.patchAlpineSeccompArm64()
+
                                         if(params.CommitId) {
                                             sh "git checkout ${params.CommitId}"
                                         }
--- vars/edgeXBuildGoParallel.groovy
+++ vars/edgeXBuildGoParallel.groovy
@@ -282,6 +282,8 @@ def call(config) {
                             stage('Prep') {
                                 steps {
                                     script {
+                                        edgex.patchAlpineSeccompArm64()
+
                                         if(params.CommitId) {
                                             sh "git checkout ${params.CommitId}"
                                         }
