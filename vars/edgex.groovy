963f7427d643a5c3c43d56d8a0f1f73a6b0488ee
Ernesto Ojeda <ernesto.ojeda@intel.com>
2022-04-27 15:59:20 -0700

    feat: add new Golang 1.17 LTS base image
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- vars/edgex.groovy
+++ vars/edgex.groovy
@@ -231,7 +231,8 @@ def getGoLangBaseImage(version, alpineBased) {
         ]
 
         def goLTSImages = [
-            '1.16': 'nexus3.edgexfoundry.org:10002/edgex-devops/edgex-golang-base:1.16-alpine-lts'
+            '1.16': 'nexus3.edgexfoundry.org:10002/edgex-devops/edgex-golang-base:1.16-alpine-lts',
+            '1.17': 'nexus3.edgexfoundry.org:10002/edgex-devops/edgex-golang-base:1.17-alpine-lts'
         ]
 
         // isLTS uses env.GIT_BRANCH to determine if the build is a LTS build
